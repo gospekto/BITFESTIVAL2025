cbe30fa7ca7434b6717a9dc201d65febdfa94089
Dominik <dominik1213141555@gmail.com>
2025-11-29 15:07:33 +0100

    kolejna poprawka

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
